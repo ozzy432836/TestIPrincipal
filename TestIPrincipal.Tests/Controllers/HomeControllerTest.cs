0dc32f74dcddc94dcad6039a21378b3f76331e8e
Andrew Osiname <osinamea@gmail.com>
2016-03-10 22:29:10 +0000

    initial

---
+++ TestIPrincipal.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,67 @@
+﻿using System.Security.Claims;
+using Moq;
+using NUnit.Framework;
+using TestIPrincipal.Controllers;
+using ClaimTypes = TestIPrincipal.Controllers.ClaimTypes;
+
+namespace TestIPrincipal.Tests.Controllers
+{
+    [TestFixture]
+    public class HomeControllerTest
+    {
+
+        public class AdminHasAccessToResourceTests : HomeControllerTest
+        {
+            private HomeController _controller;
+            private Mock<ClaimsPrincipal> _mockUser;
+            private ClaimsPrincipal _user;
+            private readonly string _adminsResourceId = "3.14";
+            private string _aDifferentAdminsResourceId = "3.141";
+            private bool _verdict;
+
+            [SetUp]
+            public void SetUp()
+            {
+                _controller = new HomeController();
+
+                _mockUser = new Mock<ClaimsPrincipal>();
+
+                _mockUser
+                    .Setup(m => m.HasClaim(ClaimTypes.Resource, _adminsResourceId))
+                    .Returns(true);
+
+                _user = _mockUser.Object;
+                _controller.User = _user;
+            }
+
+            [Test]
+            public void AdminCanAccessResourcesForTheirOwnResourceId()
+            {
+                _verdict = _controller.AdminHasAccessToResource(_adminsResourceId);
+                Assert.AreEqual(true, _verdict);
+            }
+
+            [Test]
+            public void AdminCannotAccessResourcesForSomeoneElsesResouceId()
+            {
+                _verdict = _controller.AdminHasAccessToResource(_aDifferentAdminsResourceId);
+                Assert.AreEqual(false, _verdict);
+            }
+
+            [TestCase("3.14")]
+            [TestCase("3.141")]
+            [TestCase("3.1415")]
+            [TestCase("3.14159")]
+            [TestCase("3.141593")]
+            public void SuperAdminCanAccessResoucesForAnyResouceId(string resourceId)
+            {
+                _mockUser
+                    .Setup(m => m.HasClaim(ClaimTypes.SuperAdmin, "1"))
+                    .Returns(true);
+
+                _verdict = _controller.AdminHasAccessToResource(resourceId);
+                Assert.AreEqual(true, _verdict);
+            }
+        }
+    }
+}
---
+++ TestIPrincipal/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace TestIPrincipal.Controllers
+{
+    public class HomeController : ApiController
+    {
+        public ActionResult Index()
+        {
+            return null;
+        }
+
+        public bool AdminHasAccessToResource(string resouceId)
+        {
+            return User.IsInternalAdmin() || User.HasAccessToResource(resouceId);
+        }
+    }
+    
+    public static class IPrincipalUserExtensions
+    {
+        public static bool IsInternalAdmin(this IPrincipal user)
+        {
+            var claimsPrincipal = user.ToClaimsPrincipal();
+            return claimsPrincipal.HasClaim(ClaimTypes.SuperAdmin, "1");
+        }
+
+        public static bool HasAccessToResource(this IPrincipal user, string resouceId)
+        {
+            var claimsPrincipal = user.ToClaimsPrincipal();
+            return claimsPrincipal.HasClaim(ClaimTypes.Resource, resouceId);
+        }
+
+        public static ClaimsPrincipal ToClaimsPrincipal(this IPrincipal user)
+        {
+            var claimsPrincipal = user as ClaimsPrincipal;
+
+            if (claimsPrincipal == null)
+            {
+                throw new Exception("No Claims Principal");
+            }
+
+            return claimsPrincipal;
+        }
+    }
+
+    public static class ClaimTypes
+    {
+        public static string SuperAdmin = "SuperAdmin";
+        public static string Resource = "Resource";
+    }
+}
